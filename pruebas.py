458e5cd709cbd4b8f656e1c0d17ac8923431c743
Edilberto <edipes82@gmail.com>
2023-02-17 21:22:23 -0500

    Nuevos cambios

--- pruebas.py
+++ pruebas.py
@@ -9,3 +9,5 @@ which of us ever undertakes laborious physical exercise, except to obtain some a
 has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, 
 or one who avoids a nice that produces no resultant pleasure?
 that produces no resultant pleasure?" 
+
+Cambios de la nueva rama...
